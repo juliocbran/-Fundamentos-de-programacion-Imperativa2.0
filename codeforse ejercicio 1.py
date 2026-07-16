56f2975b1095dcebdfb523c541d89a4fce646547
juliocbran <vidal.julio@correounivalle.edu.co>
2026-07-15 23:12:53 -0500

    organizar estructura
    el Código le carecia de una extructura clara, esta nueva version corrige eso.

--- codeforse ejercicio 1.py
+++ codeforse ejercicio 1.py
@@ -1,11 +1,17 @@
 
-#pedimos multiples datos en un solo input
-n,m,a = map(int,input("Ingrese la Base de la losas:").split())
+#pedimos multiples datos en un solo input 
+
+
+n,m = map(int,input("Ingrese las medidas N x M de La Plaza (separado por espacio):").split())
+a = int(input("Ingrese el largo o ancho de la losas:"))
+
 
 #calculamos alto y ancho total
 ancho, alto = (n+a - 1) // a, (m+a-1)//a
-
-print(id(ancho), id(alto))
 print(ancho * alto)
+
+#prueba de consumo de memoria
+print(id(ancho), id(alto))
+
 
 
